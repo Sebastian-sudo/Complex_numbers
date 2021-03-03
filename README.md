a9a6fda213945d872d13de4041ccbac1c45c976e
Sebastian Mazurek <smazurek.sm@gmail.com>
2021-03-03 16:55:55 +0100

    added README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Complex_numbers
+## Complex_numbers
+
+Calculator for complex numbers, addition, printing, graphs, .......
+
+## Requirements
+
+g++ compiler
+
+## Usage
+
+```
+make
+./Complex_numbers
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- header.h
+++ header.h
@@ -2,17 +2,57 @@
 #define HEADERS
 
 #include <iostream>
+#include <string>
 
 class Complex {
 private:
 	double real, imag;
 public:
 	Complex();
+	Complex(double real, double imag);
+	~Complex() {}
+	double getReal() const { return this->real; }
+	double getImag() const { return this->imag; }
+	Complex & operator=(const Complex &);
+	const Complex operator+(const Complex &);
+	Complex & operator++(void);
+	Complex operator++(int);
+	friend std::ostream& operator<<(std::ostream& os, const Complex&);
+	friend std::istream& operator>>(std::istream& is, Complex&);
 };
 
 Complex::Complex() {
 	this->real = 0.0;
 	this->imag = 0.0;
+}
+
+Complex::Complex(double real, double imag) {
+	this->real = real;
+	this->imag = imag;
+}
+
+Complex & Complex::operator=(const Complex &) {
+
+}
+
+const Complex Complex::operator+(const Complex &) {
+
+}
+
+Complex & Complex::operator++(void) {
+
+}
+
+Complex Complex::operator++(int) {
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& temp) {
+
+}
+
+std::istream& operator>>(std::istream& is, Complex& temp) {
+
 }
 
 #endif
